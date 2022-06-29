0f2d91d1059e598eb2b24a869d12ba3be269b01e
mcarolina2 <108403441+mcarolina2@users.noreply.github.com>
2022-06-29 08:15:09 -0300

    Create register.php

---
+++ register.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Primeiro Acesso</h1>
+    <form action="save-user.php" method="POST">
+        <input type="text" name="nome" placeholder="Nome">
+        <input type="text" name="email" placeholder="Email">
+        <input type="password" name="senha" placeholder="Senha"> 
+        <input type="text" name="cpf" placeholder="CPF">
+        <input type="text" name="telefone" placeholder="Telefone">
+        <input type="date" name="datadenascimento" placeholder="Data De Nascimento">
+        <input type="text" name="cep" placeholder="CEP">
+        <input type="submit">
+    </form>
+</body>
+</html>
