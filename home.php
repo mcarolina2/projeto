8ed9d150054f5e8819cfc1dd6a79b53e8a3893ca
mcarolina2 <108403441+mcarolina2@users.noreply.github.com>
2022-07-06 09:50:32 -0300

    Add files via upload

--- home.php
+++ home.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION['email'])) {
 function is_logged() {
     return isset($_SESSION['user_id']);
 }
-?>
+?> 
 
 <!DOCTYPE html>
 <html lang="en">
@@ -21,15 +21,101 @@ function is_logged() {
 
 </head>
 <body>
-    <?php if(is_logged()): ?>
-    <h1>
+<h1>
         Anúncios
     </h1>
-    
+    <?php if(is_logged()):?> 
+<?php
+$user_id = $_SESSION['user_id'];
+
+$dsn = 'mysql:dbname=test;port=3307';
+$pdo = new PDO($dsn, 'root','root');
+$stmt = $pdo->query("
+    select * from anuncios 
+    where user_id != $user_id
+    order by ano , titulo
+");
+$data = $stmt->fetchAll();
+?> 
+<table>
+        <?php foreach ($data as $row): ?>
+            <tr>
+                <td><?= $row['titulo'] ?></td>
+                <td><?= $row['ano'] ?></td>
+                <td><?= $row['descricao'] ?></td>
+
+                </td></tr>
+        <?php endforeach ?>
+    </table>
    
+<!--
     <a href="logout.php">sair</a>
     <a href="livros/index.php">Ver meus Anúncios </a>
-    <?php endif ?>
-</body>
+     -->
 
+    <?php endif ?>
+
+    <header> 
+        <ul>
+            <li>
+                <a href="home.php"> 
+            <span class="icon"></span> 
+            <span class="icon">Home</span> 
+            </li> 
+
+            <li>
+                <a href=""> 
+            <span class="icon"></span> 
+            <span class="icon">Usuario</span> 
+            </li> 
+
+            <li>
+                <a href="livros/index.php"> 
+            <span class="icon"></span> 
+            <span class="icon">Meus Anúncios</span> 
+            </li>
+
+            <li>
+                <a href=""> 
+            <span class="icon"></span> 
+            <span class="icon">Configuração</span> 
+            </li> 
+
+            <li>
+                <a href=""> 
+            <span class="icon"></span> 
+            <span class="icon">Duvidas</span> 
+            </li> 
+
+            <li>
+                <a href=""> 
+            <span class="icon"></span> 
+            <span class="icon">Ajuda</span> 
+            </li> 
+
+            <li>
+                <a href="logout.php"> 
+            <span class="icon"></span> 
+            <span class="icon">Sair</span> 
+            </li>
+
+        
+        </ul>
+    </header>
+<style>
+    header{ 
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+   
+}
+    
+
+</style> 
+
+
+
+
+</body>
 </html>
